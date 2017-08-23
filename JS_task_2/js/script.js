1c88bf4cd2921fb6287b1bad750610faa40f9ace
Alex Kutkanych <alexander.kutkanych@gmail.com>
2017-08-23 10:03:04 +0300

    back to main page btn added

--- JS_task_2/js/script.js
+++ JS_task_2/js/script.js
@@ -1,5 +1,26 @@
-//add developers names on top of pictures
+var mainPage = document.querySelector("#main-page");
+var profilePage = document.querySelector("#profile-page");
+profilePage.classList.add("profile-page_hide");
 
+var developersGrid = document.querySelector("#developers-grid");
 
+function showDevelopersProfile(e){
+	if(e.target != e.currentTarget){
+		var clickedItem = e.target.id;
+		mainPage.classList.add("main-page_hide");
+		profilePage.classList.remove("profile-page_hide");
+	}
+	e.stopPropagation();
+}
 
-// addDevelopersNameFromDB();
+developersGrid.addEventListener("click", showDevelopersProfile, false);
+
+//to main page
+
+function showMainPage(){
+	mainPage.classList.remove("main-page_hide");
+	profilePage.classList.add("profile-page_hide");
+}
+
+var toMainPageBtn = document.querySelector("#to-main-page");
+toMainPageBtn.addEventListener("click", showMainPage);
