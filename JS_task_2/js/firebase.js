8a4277001cbf89366c76015f4ad9c727837e9c6f
Oleksandr Kutkanych <Oleksandr_Kutkanych@epam.com>
2017-08-25 18:22:04 +0300

    handlesbar JS v1.2 first page is rendered

--- JS_task_2/js/firebase.js
+++ JS_task_2/js/firebase.js
@@ -33,32 +33,34 @@ firebase.initializeApp(config);
 
 
 var developersGrid = document.querySelector("#developers-grid");
-
+var developersName = document.querySelectorAll(".developer-name");
+var developersPicture = document.querySelectorAll(".developer-picture");
 // var test = document.querySelector("#object");
 
 //handlebars on JS
 
-var hadlebarsTest = document.querySelector("#test").innerHTML;
+var hadlebarsTest = document.querySelector("#experience-block").innerHTML;
 var compiledTest = Handlebars.compile(hadlebarsTest);
 var experienceBl = document.querySelector(".experience-block");
 
 var dBRef = firebase.database().ref();
+
 dBRef.on("value", function(snap) {
     var usersInfo = snap.val();
     experienceBl.innerHTML = compiledTest(usersInfo);
-    console.log(usersInfo);
+    console.log(usersInfo.users[0].name);
 
     //connecting developers pictures to info in dbs
 
-    var developersName = document.querySelectorAll(".developer-name");
-    var developersPicture = document.querySelectorAll(".developer-picture");
+
 
     for(var i = 0; i < developersName.length; i++){
         developersName[i].innerHTML = usersInfo.users[i].name;
         developersPicture[i].setAttribute("data-name", usersInfo.users[i].name);
     }
 
-});
+    //test
+
 
 
 
@@ -67,11 +69,31 @@ dBRef.on("value", function(snap) {
             var clickedItem = e.target.id;
             mainPage.classList.add("main-page_hide");
             profilePage.classList.remove("profile-page_hide");
+
+
+            var test1 = document.querySelector("#education");
+            test1.innerHTML = e.target.getAttribute("data-name");
         }
         e.stopPropagation();
     };
 
     developersGrid.addEventListener("click", showDevelopersProfile, false);
+
+});
+
+
+Handlebars.registerHelper('listItem', function (from, to, context, options){
+    var item = "";
+    for (var i = from, j = to; i < j; i++) {
+        item = item + options.fn(context[i]);
+    }
+    return item;
+});
+
+
+
+
+
 
 
 
