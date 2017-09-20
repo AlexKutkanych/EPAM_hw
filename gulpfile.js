6a44df56f04c3745230d8bed1e55f2d1e4283c7b
Oleksandr Kutkanych <Oleksandr_Kutkanych@epam.com>
2017-09-20 10:54:51 +0300

    all basic markup added

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concatCss = require('gulp-concat-css'),
     watch = require('gulp-watch');
+var sprite = require('gulp-sprite-generator');
 
 gulp.task('concat-css', function () {
   return gulp.src('css/dist/**/*.css')
@@ -13,6 +14,21 @@ gulp.task('sass', function () {
   return gulp.src('css/src/*/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css/dist'));
+});
+
+gulp.task('sprites', function() {
+    var spriteOutput;
+
+    spriteOutput = gulp.src("./css/src/*.css")
+        .pipe(sprite({
+            baseUrl:         "./img/",
+            spriteSheetName: "sprite.png",
+            spriteSheetPath: "/img"
+        })
+    )
+
+    spriteOutput.css.pipe(gulp.dest("./dist/css"));
+    spriteOutput.img.pipe(gulp.dest("./dist/image"));
 });
 
 
