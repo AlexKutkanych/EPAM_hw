5a2c3a4d4451dacad9b9f7d0d9562d9ebd5d2c88
Oleksandr Kutkanych <Oleksandr_Kutkanych@epam.com>
2017-09-20 14:19:49 +0300

    new font added

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concatCss = require('gulp-concat-css'),
+    spritesmith = require('gulp.spritesmith'),
     watch = require('gulp-watch');
-var sprite = require('gulp-sprite-generator');
+
 
 gulp.task('concat-css', function () {
   return gulp.src('css/dist/**/*.css')
@@ -16,25 +17,20 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./css/dist'));
 });
 
-gulp.task('sprites', function() {
-    var spriteOutput;
 
-    spriteOutput = gulp.src("./css/src/*.css")
-        .pipe(sprite({
-            baseUrl:         "./img/",
-            spriteSheetName: "sprite.png",
-            spriteSheetPath: "/img"
-        })
-    )
-
-    spriteOutput.css.pipe(gulp.dest("./dist/css"));
-    spriteOutput.img.pipe(gulp.dest("./dist/image"));
+gulp.task('sprite', function () {
+    var spriteData = gulp.src('img/src/*.png').pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.css'
+    }));
+    return spriteData.pipe(gulp.dest('img/'));
 });
 
 
 gulp.task('watch', function() {
   gulp.watch('css/src/*/*.scss', ['sass']);
   gulp.watch('css/dist/*/*.css', ['concat-css']);
+  gulp.watch('img/src/*.png', ['sprite']);
 });
 
 
