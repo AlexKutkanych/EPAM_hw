cb4cd20f37a74799a47318649c7d420e90fb137c
Oleksandr Kutkanych <Oleksandr_Kutkanych@epam.com>
2017-09-25 16:47:59 +0300

    sign in pop up

--- learncode.academy/src/Header.js
+++ learncode.academy/src/Header.js
@@ -11,35 +11,55 @@ import Teams from './Teams';
 import MainPage from './MainPage';
 import Scores from './Scores';
 import Players from './Players';
+import SignInForm from './SignInForm';
 import NotFound from './NotFound';
 
 
 
 export default class Header extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isShown: false
+        }
+
+        this.showModal = this.showModal.bind(this);
+    }
+
+    showModal(){
+        const showModal = this.state.isShown == true ? false : true
+        this.setState({
+          isShown: showModal
+        })
+    }
+
 
     handleChange(e){
         const title = e.target.value;
        this.props.changeTitle(title);
     }
 
-
-
     render(){
+
+        const navBar = ["Scores", "Teams", "Players"];
 
         const test = {
             backgroundColor: "green"
+         };
 
-         }
+         const navLinks = navBar.map((page, index) => {
+             return (
+                 <a href={"/" + page} key={index} className="menu-item">{page}</a>)});
+
         return (
-
-
             <div>
-                <ul className="menu">
-                    <a href="/" className="menu-item">Home</a>
-                    <a href="/Scores" className="menu-item" >Scores</a>
-                    <a href="/Teams" className="menu-item">Teams</a>
-                    <a href="/Players" className="menu-item">Players</a>
-                </ul>
+                <nav className="menu">
+                    <a href="/"><img className="menu__logo" src="http://a.espncdn.com/combiner/i?img=/i/teamlogos/leagues/500/nba.png?w=100&h=100&transparent=true" alt="logo"/></a>
+                    {navLinks}
+                    <a href="#" onClick={this.showModal}>Sign In</a>
+                    {this.state.isShown ? <SignInForm /> : false}
+                </nav>
             <Switch>
                 <Route activeClassName="test" exact path='/' component={MainPage}/>
                 <Route activeClassName="test" exact path='/Teams' component={Teams}/>
@@ -53,4 +73,7 @@ export default class Header extends Component{
         )
 
     }
+
+
+
 }
