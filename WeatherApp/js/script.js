60558add81765587f0db9de06781a4572055bf3c
Alex <alexander.kutkanych@gmail.com>
2017-09-29 15:21:11 +0300

    Celcius to Fh convertation added

--- WeatherApp/js/script.js
+++ WeatherApp/js/script.js
@@ -1,23 +1,32 @@
 var result = document.querySelector("#result");
+var body = document.querySelector("body");
 
 if(!navigator.geolocation){
 	var denied = document.createElement("p");
 	denied.innerHTML = "Your browser does not support geolocation";
 	result.appendChild(denied);
-}
+};
 
 navigator.geolocation.getCurrentPosition(function(position){
+	function check (error) { 
+  if (error.code == error.PERMISSION_DENIED)
+      console.log("you denied me :-(");
+};
+
 	var longitude = position.coords.longitude;
 	var latitude = position.coords.latitude;
 
 	//show info on page
+	
 	var wLocation = document.querySelector("#w-location");
 	var wIcon = document.querySelector("#w-icon");
 	var wDescription = document.querySelector("#w-desc");
 	var wTemperature = document.querySelector("#w-temp");
 	var wHumidity = document.querySelector("#w-humidity");
 	var wWind = document.querySelector("#w-wind");
-	var body = document.querySelector("body");
+	var tCelcius = document.querySelector("#temp-celc");
+	var tFahrenheit = document.querySelector("#temp-fahr");
+	
 
 	fetch('https://fcc-weather-api.glitch.me/api/current?lon=' + longitude + '&lat=' + latitude)
 		.then(function(res){
@@ -26,9 +35,28 @@ navigator.geolocation.getCurrentPosition(function(position){
 				wLocation.innerHTML = data.name + ", " + data.sys.country;
 				wIcon.setAttribute("src", data.weather[0].icon);
 				wDescription.innerHTML = data.weather[0].main;
-				wTemperature.innerHTML = Math.round(data.main.temp) +" &#8451;";
+				wTemperature.innerHTML = Math.round(data.main.temp);
 				wHumidity.innerHTML = "<span class='brand-style'>Humidity:</span> " + Math.round(data.main.humidity) + "%";
 				wWind.innerHTML = "<span class='brand-style'>Wind:</span> " + data.wind.speed + "km/h";
+
+				//switch btw Celc and Fahr
+
+				function showTCelcius(){
+					wTemperature.innerHTML = Math.round(data.main.temp);
+					tFahrenheit.classList.remove("temp-active");
+					tCelcius.classList.add("temp-active");
+				}
+
+
+				function showTFahrenheit(){
+					var tFahr = Math.round(data.main.temp * 1.8 + 32);
+					wTemperature.innerHTML = tFahr;
+					tFahrenheit.classList.add("temp-active");
+					tCelcius.classList.remove("temp-active");
+				}
+
+				tCelcius.addEventListener("click", showTCelcius);
+				tFahrenheit.addEventListener("click", showTFahrenheit);
 
 				if(data.weather[0].main === "Clouds") {
 					body.style.backgroundImage = 'url("img/clouds.jpeg")';
