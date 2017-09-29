2e6e5d969300a89de04c16703848c3da96f3418b
Alex <alexander.kutkanych@gmail.com>
2017-09-29 23:42:39 +0300

    WikipediaViewer ready

--- Wikipedia Viewer/js/script.js
+++ Wikipedia Viewer/js/script.js
@@ -9,28 +9,36 @@ function showSearchBlock(){
 searchBlockBtn.addEventListener("click", showSearchBlock);
 
 $(function(){
-
 		
-	$(".search-btn").click(function(){
+	$(".search-btn").click(function(e){
 
 		var searchQuery = $("#search-input").val();
-		console.log(searchQuery);
+		if(searchQuery.length !== 0 || searchQuery !== ''){
+			console.log(searchQuery);
+			$.ajax({
+				type: "GET",
+				url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchQuery + "&prop=revisions&rvprop=content&format=json&callback=?",
+				dataType: "json",
+		 		async: false,
+		 		success: function(data){
+		 			console.log(data);
+		 			for (var i = 0; i < data[1].length; i++){
+		 				$("#results").append("<article class='results-article'><a class='article-title' href=" + data[3][i] + ">" + data[1][i] + "</a><p class='article-desc'>" + data[2][i]+ "</p></article>");
 
-		$.ajax({
-			type: "GET",
-			url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchQuery + "&prop=revisions&rvprop=content&format=json&callback=?",
-			dataType: "json",
-	 		async: false,
-	 		success: function(data){
-	 			console.log(data);
-	 			// $("#result").append("<article><></article>")
-	  				
-	 		},
-	 		error: function(errorMsg){
-	 			console.log("Error occured " + errorMsg);
-	 		}
-		})
-		
+		 				if(data[2][i] === ""){
+		 					console.log($(this).parent());
+		 				}
+		 			}
+		  				
+		 		},
+		 		error: function(errorMsg){
+		 			console.log("Error occured " + errorMsg);
+		 		}
+			})
+		} else {
+			alert("Enter the thing you are looking for");
+		}
+
 	})
 })
 	
