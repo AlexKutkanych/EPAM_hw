cc76116c5272eb86a5c6e739995b2ddd0901cb1f
Alex <alexander.kutkanych@gmail.com>
2017-09-29 11:24:15 +0300

    not found modified

--- learncode.academy/src/Header/Header.js
+++ learncode.academy/src/Header/Header.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:learncode.academy/src/Header/Header.js
 import React, { Component } from 'react';
 import {
     Route,
@@ -78,3 +79,90 @@ export default class Header extends Component{
 
 
 }
+=======
+import React, { Component } from 'react';
+import {
+    Route,
+    Switch,
+    NavLink,
+    Link
+} from 'react-router-dom';
+
+import './App.css';
+import './index.css';
+import Teams from './Teams';
+import MainPage from './MainPage';
+import Scores from './Scores';
+import Players from './Players';
+import News from './News';
+import SignInForm from './SignInForm';
+import NotFound from './NotFound';
+
+
+
+
+export default class Header extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isShown: false,
+            name: "Will"
+        }
+
+        this.showModal = this.showModal.bind(this);
+    }
+
+    showModal(){
+        const showModal = this.state.isShown == true ? false : true;
+        this.setState({
+          isShown: showModal
+        })
+    }
+
+
+    handleChange(e){
+        const title = e.target.value;
+       this.props.changeTitle(title);
+    }
+
+    render(){
+
+        const navBar = ["Scores", "Teams", "Players", "News"];
+
+        const test = {
+            backgroundColor: "green"
+         };
+
+         const navLinks = navBar.map((page, index) => {
+             return (
+                 <NavLink activeClassName="test" to={"/" + page} key={index} className="menu-item" >{page}</NavLink>)});
+
+
+        return (
+            <div>
+                <nav className="menu">
+                    <Link to="/"><img className="menu__logo" src="http://a.espncdn.com/combiner/i?img=/i/teamlogos/leagues/500/nba.png?w=100&h=100&transparent=true" alt="logo"/></Link>
+                    {navLinks}
+
+                    <a href="#" onClick={this.showModal}>Sign In</a>
+                    {this.state.isShown ? <SignInForm /> : false}
+
+                </nav>
+                <Switch>
+                    <Route exact path='/' component={MainPage}/>
+                    <Route exact path='/Teams' component={Teams}/>
+                    <Route exact path='/Scores' component={Scores}/>
+                    <Route exact path='/Players' component={Players}/>
+                    <Route exact path='/News' component={News}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </div>
+
+        )
+    }
+
+
+
+}
+>>>>>>> Stashed changes:learncode.academy/src/Header.js
--- learncode.academy/src/NotFound.jsx
+++ learncode.academy/src/NotFound.jsx
@@ -5,7 +5,11 @@ export default class NotFound extends React.Component{
     render() {
 
         return(
+<<<<<<< Updated upstream
             <img src="https://www.goldsmithssu.org/pageassets/yourunion/staff/404-Face-not-found.png" alt="not found pic" />
+=======
+                    <img src="https://www.goldsmithssu.org/pageassets/yourunion/staff/404-Face-not-found.png" />
+>>>>>>> Stashed changes
         )
     }
 }
