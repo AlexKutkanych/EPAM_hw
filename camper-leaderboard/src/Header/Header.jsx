0f93a69b507508407bf8cc2a10275f6c63c69590
Alex <alexander.kutkanych@gmail.com>
2017-12-17 20:33:19 +0200

    camper leaderboard envirounment is setup

---
+++ camper-leaderboard/src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <header className="header">
+          <h1 className="header__title">Camper leaderboard</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
