83bfc56e35c630f07d039f035064cc6887f3927c
Oleksandr Kutkanych <Oleksandr_Kutkanych@epam.com>
2017-12-20 15:48:20 +0200

    recipies length validation, default message set

--- Recipe-box/src/RecipeContainer/RecipeContainer.jsx
+++ Recipe-box/src/RecipeContainer/RecipeContainer.jsx
@@ -4,6 +4,7 @@ import Button from '../Button/Button';
 
 import RecipeList from '../RecipeList/RecipeList.jsx';
 import ModalBox from '../ModalBox/ModalBox.jsx';
+import NoRecipies from './NoRecipies/NoRecipies.jsx';
 import ContentAddCircleOutline from 'material-ui/svg-icons/content/add-circle-outline';
 // import ActionDelete from 'material-ui/svg-icons/action/delete';
 // import EditorModeEdit from 'material-ui/svg-icons/editor/mode-edit';
@@ -14,6 +15,7 @@ class RecipeContainer extends Component {
     super(props);
     this.state = {
       openNewRecipeModal: false,
+      recipiesEmpty: true,
       recipes: [],
       newRecipe: {}
     }
@@ -23,27 +25,35 @@ class RecipeContainer extends Component {
     this.getRecipesFromLocalStorage();
   }
 
-  setRecipesToLocalStorage = () => {
-    const Recipes = [
-      {name: "Borshch", recipe: "Borshch", ingredients: "blalblabla"},
-      {name: "asd", recipe: "asd", ingredients: "blalblabla"},
-      {name: "dgfhfgh", recipe: "dgfhfgh", ingredients: "blalblabla"},
-      {name: "ertertert", recipe: "ertertert", ingredients: "blalblabla"}
-    ];
-
-    const jsonRecipes = JSON.stringify(Recipes);
-
-    localStorage.setItem("myRecipes", jsonRecipes);
-    console.log(jsonRecipes);
-  }
+  // setRecipesToLocalStorage = () => {
+  //   const Recipes = [
+  //     {name: "Borshch", recipe: "Borshch", ingredients: "blalblabla"},
+  //     {name: "asd", recipe: "asd", ingredients: "blalblabla"},
+  //     {name: "dgfhfgh", recipe: "dgfhfgh", ingredients: "blalblabla"},
+  //     {name: "ertertert", recipe: "ertertert", ingredients: "blalblabla"}
+  //   ];
+  //
+  //   const jsonRecipes = JSON.stringify(Recipes);
+  //
+  //   localStorage.setItem("myRecipes", jsonRecipes);
+  //   console.log(jsonRecipes);
+  // }
 
   getRecipesFromLocalStorage = () => {
     const myRecipes = localStorage.getItem("myRecipes");
-    const recipes = JSON.parse(myRecipes);
-    this.setState({
-      ...this.state,
-      recipes: recipes
-    })
+    if(myRecipes){
+      const recipes = JSON.parse(myRecipes);
+      this.setState({
+        ...this.state,
+        recipes: recipes,
+        recipiesEmpty: false
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        recipiesEmpty: true
+      });
+    }
   }
 
 
@@ -61,7 +71,7 @@ class RecipeContainer extends Component {
        newRecipe: {
          ...prevState.newRecipe,
          [target.name]: target.value
-       }
+       },
      }))
    }
 
@@ -70,7 +80,8 @@ class RecipeContainer extends Component {
        recipes: [
           ...prevState.recipes,
           newRec
-       ]
+       ],
+       recipiesEmpty: false
      }));
      this.handleClose();
      return this.state;
@@ -84,6 +95,7 @@ class RecipeContainer extends Component {
       <div className="recipe-wrapper">
         <div className="recipe-container">
             <RecipeList recipes={this.state.recipes} newRecipe={this.state.newRecipe}/>
+            {this.state.recipiesEmpty && <NoRecipies />}
         </div>
         <button onClick={this.test}>Click</button>
         <Button bgColor={'#40C4FF'}
--- Recipe-box/src/RecipeList/RecipeList.jsx
+++ Recipe-box/src/RecipeList/RecipeList.jsx
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import RecipeItem from '../RecipeItem/RecipeItem.jsx';
 
 class RecipeList extends Component {
+  //
+  // componentWillReceiveProps(nextProps){
+  //   this.renderAllRecipes(nextProps.recipes);
+  // }
 
   showFullRecipe = (e) => {
      this.setState({
@@ -11,21 +15,20 @@ class RecipeList extends Component {
       console.log(e.target.dataset.name);
    }
 
-
   render() {
 
     const { recipes, newRecipe } = this.props;
 
-    const allReceipes = recipes.map((item, i) => {
-      return (<RecipeItem key={i}
-        name={item.name}
-        recipe={item.recipe}
-        ingredients={item.ingredients} />)
-    });
 
     return (
       <div>
-        {allReceipes}
+        {recipes.map((item, i) => {
+            return (<RecipeItem key={i}
+              name={item.name}
+              recipe={item.recipe}
+              ingredients={item.ingredients} />)
+          })
+        }
       </div>
     );
   }
