5f9618b053f8865d7511957edf18128461a8476a
Oleksandr Kutkanych <Oleksandr_Kutkanych@epam.com>
2017-12-20 18:31:59 +0200

    ingridients list rendered and styled

--- Recipe-box/src/RecipeItem/RecipeItem.jsx
+++ Recipe-box/src/RecipeItem/RecipeItem.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from '../Button/Button';
 
 import './recipe-item.css';
 
@@ -6,14 +7,27 @@ class RecipeItem extends Component {
 
   render() {
 
-
     const { name, recipe, ingredients, toggleRecipeState, click} = this.props;
+
     return (
         <div className={"recipe-item__block " + (toggleRecipeState ? "recipe-item__block_open" : null)}
           data-name={name} onClick={click} title="Click to see full recipe"
           >
           <p className="recipe-item__name" data-name={name}>{name}</p>
-          <p className="recipe-item__name">{ingredients}</p>
+          <div className="recipe-item__ingredients">
+              <h3 className="ingredients__title">ingredients</h3>
+              <ul className="ingredients__list">
+                {ingredients.split(",").map((item, i) => <li className="ingredients-list__item" key={i}>{item}</li>)}
+              </ul>
+              <div className="ingredients__buttons-wrapper">
+                <Button bgColor={'#FF5722'}
+                        labelText={'Delete'}
+                        click={this.handleOpen}/>
+                <Button bgColor={'#5E35B1'}
+                        labelText={'Edit'}
+                        click={this.handleOpen}/>
+              </div>
+          </div>
         </div>
     );
   }
